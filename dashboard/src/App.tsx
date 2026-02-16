dba237009c9e05457359c2ff83143ef57c1c34ba
testgithubritgu <swapnilraut1698@gmail.com>
2026-02-16 07:26:08 +0000

    adding text

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
 	<div className="App">
 	  <FrappeProvider>
-		<div>
+		<div>dfsdf
 	  <div>
 		<a href="https://vitejs.dev" target="_blank">
 		  <img src="/vite.svg" className="logo" alt="Vite logo" />
