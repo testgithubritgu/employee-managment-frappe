5a965f0d6d6825ab63381a65980dc4cb78a92014
testgithubritgu <swapnilraut1698@gmail.com>
2026-02-21 07:48:07 +0000

    remove the useused logs

--- dashboard/src/features/landing-page/Home.tsx
+++ dashboard/src/features/landing-page/Home.tsx
@@ -58,8 +58,6 @@ const Home = () => {
   // this is first stash 
 
   // this is secont 
-
-  // this is third 
   const handleUpload = async () => {
     if (!file) return alert("Select image first");
 
