937b7243c5caa3664f8ac5fad4213163767da7e1
testgithubritgu <swapnilraut1698@gmail.com>
2026-02-21 06:34:58 +0000

    space removing

--- dashboard/src/features/landing-page/Home.tsx
+++ dashboard/src/features/landing-page/Home.tsx
@@ -18,12 +18,9 @@ import { Input } from "../../components/ui/input.js"
 
 // this is first stash 
 
-<<<<<<< Updated upstream
 // this is second stash 
 
 // this is 3rd stash 
-=======
->>>>>>> Stashed changes
 
 export const uploadImage = async (file: File) => {
 
