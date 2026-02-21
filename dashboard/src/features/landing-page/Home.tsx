c6974b227cfe53298160dceee777f02e78d89037
testgithubritgu <swapnilraut1698@gmail.com>
2026-02-21 05:51:20 +0000

    stash apply 2

--- dashboard/src/features/landing-page/Home.tsx
+++ dashboard/src/features/landing-page/Home.tsx
@@ -16,8 +16,11 @@ import { Input } from "../../components/ui/input.js"
 //CHECKING STASH..........
 // 1
 
+<<<<<<< Updated upstream
 // 2 
 
+=======
+>>>>>>> Stashed changes
 export const uploadImage = async (file: File) => {
 
   if (!file) return;
