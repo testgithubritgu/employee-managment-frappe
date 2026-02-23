d8467d019de829af032472f01b86290634b2a088
testgithubritgu <swapnilraut1698@gmail.com>
2026-02-23 06:55:19 +0000

    fix navbar issue

--- dashboard/src/components/commen/Navbar.tsx
+++ dashboard/src/components/commen/Navbar.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 import { useState } from "react";
 
 export default function Navbar(){
@@ -9,29 +10,30 @@ export default function Navbar(){
             <div className="max-w-7xl mx-auto px-6">
                 <div className="flex items-center justify-between h-16">
 
-                    {/* Logo */}
                     <div className="text-xl font-bold ">
                         Dev<span className="text-indigo-600">Brand</span>
                     </div>
 
-                    {/* Desktop Menu */}
+                   
                     <div className="hidden md:flex items-center space-x-8">
+                        <SignedOut>
+                            <SignInButton />
+                            <SignUpButton />
+                        </SignedOut>
+                        <SignedIn>
                         {["Home", "Features", "Pricing", "Contact"].map((item) => (
                             <a
-                                key={item}
-                                href="#"
-                                className="text-gray-600 hover:text-indigo-600 transition font-medium"
+                            key={item}
+                            href="#"
+                            className="text-gray-600 hover:text-indigo-600 transition font-medium"
                             >
                                 {item}
                             </a>
                         ))}
-
-                        <button className="px-4 py-2 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700 transition">
-                            Login
-                        </button>
+                        <UserButton />
+                      
+                        </SignedIn>
                     </div>
-
-                    {/* Mobile Button */}
                     <button
                         onClick={() => setOpen(!open)}
                         className="md:hidden text-gray-700"
@@ -52,8 +54,6 @@ export default function Navbar(){
                     </button>
                 </div>
             </div>
-
-            {/* Mobile Menu */}
             {open && (
                 <div className="md:hidden bg-white border-t border-gray-200">
                     <div className="px-6 py-4 space-y-4">
--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -7,21 +7,31 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter } from 'react-router-dom'
 import ErrorBoundary from './app-boundries/Error.tsx'
-
+import { ClerkProvider } from "@clerk/clerk-react"
 const queryClient = new QueryClient()
 
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+
 
     <BrowserRouter>
       <FrappeProvider>
         <QueryClientProvider client={queryClient}>
+
           <App />
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </FrappeProvider>
     </BrowserRouter>
+  </ClerkProvider>
     </ErrorBoundary>
   </StrictMode>,
 )
